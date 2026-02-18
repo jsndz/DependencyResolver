6d0ef7a565a9eaf5dc807a2addd916e78b8508e5
jsndz <jaisondz9360@gmail.com>
2026-02-18 11:56:23 +0530

    home page components

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,3 +1,11 @@
+import { Footer } from "../components/home/Footer";
+import { Hero } from "../components/home/Hero";
+import { Navbar } from "../components/ui/resizable-navbar";
+
 export default function HomePage() {
-  return <div></div>;
+  return <div>
+    <Navbar><div></div></Navbar>
+    <Hero></Hero>
+    <Footer></Footer>
+  </div>;
 }
